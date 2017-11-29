2b033c41f3bfbc7c38322b5b421962675e02e73c
肥皂泡上尉 <1012434956@qq.com>
2017-11-29 08:54:22 +0800

    ..

---
+++ WowCarLauncher/mobile/src/main/java/com/wow/carlauncher/plugin/music/common/MusicController.java
@@ -0,0 +1,15 @@
+package com.wow.carlauncher.plugin.music.common;
+
+/**
+ * Created by 10124 on 2017/11/16.
+ */
+
+public interface MusicController {
+    void play();
+
+    void pause();
+
+    void next();
+
+    void pre();
+}
---
+++ WowCarLauncher/mobile/src/main/java/com/wow/carlauncher/plugin/music/common/MusicView.java
@@ -0,0 +1,17 @@
+package com.wow.carlauncher.plugin.music.common;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by 10124 on 2017/11/16.
+ */
+
+public interface MusicView {
+    void refreshInfo(final String title, final String artist);
+
+    void refreshProgress(final int curr_time, final int total_time);
+
+    void refreshCover(final Bitmap cover);
+
+    void refreshState(final boolean run);
+}

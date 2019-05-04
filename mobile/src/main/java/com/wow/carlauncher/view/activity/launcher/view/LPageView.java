2e778301129991b22d66537d4d59bc95cf75c9fa
刘一先 <1012434956@qq.com>
2019-05-04 21:02:34 +0800

    fix bug

--- mobile/src/main/java/com/wow/carlauncher/view/activity/launcher/view/LPageView.java
+++ mobile/src/main/java/com/wow/carlauncher/view/activity/launcher/view/LPageView.java
@@ -88,7 +88,7 @@ public class LPageView extends BaseEXView {
                 ll_base.addView(new View(getContext()), params);
             } else {
                 int margin4 = ViewUtils.dip2px(getContext(), 4);
-                int margin10 = ViewUtils.dip2px(getContext(), 4);
+                int margin10 = ViewUtils.dip2px(getContext(), 10);
                 params.setMargins(margin4, margin10, margin4, margin4);
                 if (layoutEnum.equals(LayoutEnum.LAYOUT1)) {
                     params.setMargins(margin10, margin10, margin10, margin10);

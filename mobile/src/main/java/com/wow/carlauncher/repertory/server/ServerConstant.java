db82fecdb5b8a603450aac52cb309540a60c1bfe
刘一先 <1012434956@qq.com>
2019-05-29 10:43:50 +0800

    fix bug

--- mobile/src/main/java/com/wow/carlauncher/repertory/server/ServerConstant.java
+++ mobile/src/main/java/com/wow/carlauncher/repertory/server/ServerConstant.java
@@ -1,7 +1,7 @@
 package com.wow.carlauncher.repertory.server;
 
 public class ServerConstant {
-    public static final String SERVER_URL = "http://car-launcher.dudu-lucky.com:7000/";
+    public static final String SERVER_URL = "http://app.dudu-lucky.com:7000/";
     //public static final String SERVER_URL = "http://192.168.31.158:8101/";
 
     public static final int NET_ERROR = -1000;
